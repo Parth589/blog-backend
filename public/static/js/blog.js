2dab2afa027bdde977b592adc2602f63e1e8ef79
Parth <parthparmar4507@gmail.com>
2022-10-01 18:38:07 +0530

    ubuntu backup

---
+++ public/static/js/blog.js
@@ -0,0 +1,20 @@
+const renderContent = (html) => {
+    const container = document.getElementById('blog-container');
+    container.innerHTML = html;
+};
+
+const start = async () => {
+    const response = await fetch('/api/v1/blogs/' + location.href.substring(location.href.lastIndexOf('/') + 1));
+    const data = await response.json();
+    if (data.success) {
+        console.log(data);
+        renderContent(data.data.content);
+
+        if (data.data.thumbnail_link)
+            document.getElementById('main-image').src = data.data.thumbnail_link;
+        else
+            document.getElementById('main-image').src = 'random image link';
+
+    }
+};
+start();
--- public/static/js/blogs.js
+++ public/static/js/blogs.js
@@ -1,27 +1,28 @@
 const container = document.getElementById('blogs');
 console.log(container);
 const generateRegularCard = (data) => {
-    return `<a href="${data.link}"class="card flex-col-up ">
-                <div class="thumbnail-outer">
-                    <img src="/assets/arrow-up-right-circle-fill.svg" alt="" class="navigator icon-md filter-inv">
-                    <img src="${data.thumbnail_link}" alt=""
-                        class="thumbnail">
-                </div>
-                <div class="txt flex-col-up gap-sm-2">
-                    <em class="font-sm-2 text-regular">${data.keywords}</em>
+    return `<div class="thumbnail-outer">
+                <img src="/assets/arrow-up-right-circle-fill.svg" alt="" class="navigator icon-md filter-inv">
+                <img src="${data.thumbnail_link}" alt=""
+                    class="thumbnail">
+            </div>
+            <div class="txt flex-col-up gap-sm-2">
+                <em class="font-sm-2 text-regular">${data.keywords}</em>
 
-                    <h1 class="title text-imp font-md">${data.title.slice(0, 50)}</h1>
-                    <span class="brief text-regular font-sm-2">${data.brief.slice(0, 100)}</span>
-                </div>
-            </a>`;
+                <h1 class="title text-imp font-md">${data.title.slice(0, 50)}</h1>
+                <span class="brief text-regular font-sm-2">${data.brief.slice(0, 100)}</span>
+            </div>`;
 };
 
 
 const renderBlogs = (array) => {
     array.forEach(e => {
-        e.link = `/api/v1/blogs/${e._id}`;
-        container.innerHTML += generateRegularCard(e);
-        // * above line is causing the bug.
+        e.link = `/blog/${e._id}`;
+        const element = document.createElement('a');
+        element.href = e.link;
+        element.classList.add('card', 'flex-col-up');
+        element.innerHTML = generateRegularCard(e);
+        container.appendChild(element);
     });
 };
 const retriveAndRender = async () => {
@@ -35,8 +36,11 @@ const retriveAndRender = async () => {
 
 const load_btn = document.getElementById('load-more');
 console.log(load_btn);
-load_btn.addEventListener('click', retriveAndRender);
-// retriveAndRender();
+load_btn.addEventListener('click', () => {
+    console.log('button clicked');
+    retriveAndRender();
+});
+retriveAndRender();
 
 const column = document.querySelector('.col-1');
 document.getElementById('toggle').addEventListener('click', () => {
@@ -50,3 +54,13 @@ document.getElementById('toggle').addEventListener('click', () => {
 
 
 // search functionality
+const search = async (term) => {
+    const response = await fetch('/api/v1/blogs/search', {
+        method: 'POST',
+        body: {
+            keywords: term.trim()
+        }
+    });
+    const data = await response.json();
+    renderBlogs(data);
+};
